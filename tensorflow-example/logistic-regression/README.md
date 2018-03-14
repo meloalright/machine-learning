7077616c805ad6e1fa5486677b72ef8886a6b290
melomacbookair <redorgreen@sina.cn>
2018-03-14 10:57:24 +0800

    add handwrite logist

---
+++ tensorflow-example/logistic-regression/README.md
@@ -0,0 +1,1 @@
+`[tf.nn.softmax] 为激活函数(j=2时等价于Sigmoid)`    
---
+++ tensorflow-example/logistic-regression/handwrite-logistic.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+
+MNIST = mnist
+
+mndata = mnist
+
+sess = tf.Session()
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.matmul(x, W) + b
+y_ = tf.placeholder("float", [None, 10])
+# 使用Tensorflow自带的交叉熵函数
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# 训练
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(500)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# 选定测试集
+images = mndata.test.images
+labels = mndata.test.labels
+num = 9006
+image = images[num]
+label = labels[num]
+
+# 打印label
+print('这张图片的实际数字是: ' + str(label))
+
+# 打印图片
+dmp = np.array(image).reshape(28, 28)
+plt.imshow(dmp, cmap='gray')
+plt.show()
+
+# 测试新图片，并输出预测值
+a = np.array(image).reshape(1, 784)
+y = tf.nn.softmax(y)  # 为了打印出预测值，我们这里增加一步通过softmax函数处理后来输出一个向量
+result = sess.run(y, feed_dict={x: a})  # result是一个向量，通过索引来判断图片数字
+print('\n\n预测值为：')
+print(result)
+
+# 输出结果
+print('\n\n结果：')
+count = 0
+for i in result[0]:
+    if i > 0.2 and i < 2.01:
+        print(str(count))
+    count += 1
